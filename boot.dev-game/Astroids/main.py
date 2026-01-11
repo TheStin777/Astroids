0bbfae03d2dee23dbd406dfb19ff412128078d68
TheStin777 <king-ice-spider@proton.me>
2026-01-12 07:19:27 +0900

    Loading the screen

--- boot.dev-game/Astroids/main.py
+++ boot.dev-game/Astroids/main.py
@@ -6,8 +6,8 @@ def main():
     WIDTH = constants.SCREEN_WIDTH
     HEIGHT = constants.SCREEN_HEIGHT
     print(f"Starting Asteroids with pygame version: {VERSION}")
-    print(f"Screen width:{WIDTH}")
-    print(f"Screen height:{HEIGHT}")
+    print(f"Screen width: {WIDTH}")
+    print(f"Screen height: {HEIGHT}")
 
 
 if __name__ == "__main__":
