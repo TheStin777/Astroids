f50b3fa0ce06e23b543a1fbf4156bb036968d77b
TheStin777 <king-ice-spider@proton.me>
2026-01-12 07:37:56 +0900

    Adding Screen

--- boot.dev-game/Astroids/main.py
+++ boot.dev-game/Astroids/main.py
@@ -1,13 +1,20 @@
 import pygame
 import constants
+from logger import log_state
 
 def main():
+    pygame.init()
     VERSION = pygame.version.ver
     WIDTH = constants.SCREEN_WIDTH
     HEIGHT = constants.SCREEN_HEIGHT
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     print(f"Starting Asteroids with pygame version: {VERSION}")
     print(f"Screen width: {WIDTH}")
     print(f"Screen height: {HEIGHT}")
+
+    while log_state:
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
