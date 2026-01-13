ee1f9cce4bdda4213a1928799f08c7e7a5de4cf6
TheStin777 <king-ice-spider@proton.me>
2026-01-13 09:02:41 +0900

    Added the player to the screen

--- boot.dev-game/Astroids/main.py
+++ boot.dev-game/Astroids/main.py
@@ -1,6 +1,7 @@
 import pygame
 import constants
 from logger import log_state
+from player import *
 
 def main():
     pygame.init()
@@ -8,20 +9,25 @@ def main():
     WIDTH = constants.SCREEN_WIDTH
     HEIGHT = constants.SCREEN_HEIGHT
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
     print(f"Starting Asteroids with pygame version: {VERSION}")
     print(f"Screen width: {WIDTH}")
     print(f"Screen height: {HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
-    
+    player_x = WIDTH/2
+    player_y = HEIGHT/2
+    player = Player(player_x, player_y)
 
     while True:
         
         log_state()
+       
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = (clock.tick(60))/1000
         
